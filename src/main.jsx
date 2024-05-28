ae03f32815030ffbc8cd24a291b83fad00457187
Galuh panca <galuhpanca166@gmail.com>
2024-05-28 19:29:44 +0700

    menambahkan page product

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
+import Products from "./pages/products.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
   },
 ]);
 
---
+++ src/pages/products.jsx
@@ -0,0 +1,24 @@
+export default function Products() {
+  return (
+    <div className="w-[95%] mx-auto">
+      <div className="flex basis-2/3 justify-between min-h-screen">
+        <div className="w-full bg-slate-400">
+          <div className="flex">
+            <div className=" bg-gray-200">
+              <h1 className="text-center">hello products</h1>
+              <div className="bg-slate-300">
+                <p>img product</p>
+              </div>
+              <div>
+                <p>description product</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/3">
+          <h1>hallo</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
