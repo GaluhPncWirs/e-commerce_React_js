55025d6289de85d53f731d308291f1d76c6f9150
Galuh panca <galuhpanca166@gmail.com>
2024-05-29 09:21:18 +0700

    add image

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,16 +1,22 @@
+import reusableBag from "../assets/reusable-bag.jpg";
 export default function Products() {
   return (
     <div className="w-[95%] mx-auto">
       <div className="flex basis-2/3 justify-between min-h-screen">
         <div className="w-full bg-slate-400">
           <div className="flex">
-            <div className=" bg-gray-200">
-              <h1 className="text-center">hello products</h1>
-              <div className="bg-slate-300">
-                <p>img product</p>
+            <div className=" bg-gray-200 max-w-sm">
+              <h1 className="text-center">Reusable Totebag recycle</h1>
+              <div className="bg-slate-300 flex justify-center items-center">
+                <img src={reusableBag} alt="" className="w-2/6 " />
               </div>
               <div>
-                <p>description product</p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Fugiat voluptates temporibus debitis vitae architecto quas
+                  quia deserunt voluptatum enim, labore odit eaque! Voluptatibus
+                  placeat ratione a suscipit cumque explicabo minus.
+                </p>
               </div>
             </div>
           </div>
