21dbc4098b6f6d663be1f57c7e58b53d6c6e9969
Galuh panca <galuhpanca166@gmail.com>
2024-06-07 20:01:18 +0700

    add product price

--- src/fragment/cardProduct.jsx
+++ src/fragment/cardProduct.jsx
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom";
-
 export default function CardProduct(props) {
   const { children } = props;
   return (
-    <div className="max-w-sm bg-neutral-700 rounded-lg py-5 text-white flex flex-col justify-between">
+    <div className="max-w-xs bg-neutral-700 rounded-lg py-5 text-white flex flex-col justify-between">
       {children}
     </div>
   );
@@ -15,7 +13,7 @@ function Header(props) {
     <div>
       <h1 className="text-center font-semibold text-lg mb-5">{title}</h1>
       <div className="flex justify-center items-center mb-5">
-        <img src={image} className="w-2/3 max-h-52 rounded-2xl" />
+        <img src={image} className="w-1/2 max-h-52 rounded-2xl" />
       </div>
     </div>
   );
@@ -30,16 +28,18 @@ function Body(props) {
   );
 }
 
-function Footer({ price, toDetailProduct }) {
+function Footer({ price, handleAddToCart, id }) {
   return (
     <div className="flex justify-between items-center mt-4 px-4">
-      <h4 className="font-semibold text-xl tracking-wide">{price}</h4>
-      <Link
+      <h4 className="font-semibold text-xl tracking-wide">
+        {price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}
+      </h4>
+      <button
         className="bg-blue-500 rounded-md px-5 py-0.5 font-medium hover:bg-blue-600"
-        to={toDetailProduct}
+        onClick={() => handleAddToCart(id)}
       >
         Add To Cart
-      </Link>
+      </button>
     </div>
   );
 }
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,31 +2,55 @@ import CardProduct from "../fragment/cardProduct";
 import reusableBag from "/images/reusable-bag.jpg";
 import shoesAliodas from "/images/shoes-aliodas.jpg";
 import hat from "/images/hat.jpg";
+import { useState } from "react";
 
 export default function Products() {
   const products = [
     {
+      key: Math.random() * 100,
       id: 1,
       title: "Reusable Totebag recycle",
       image: reusableBag,
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat voluptates temporibus debitis vitae architecto quas quia deserunt voluptatum enim labore odit eaque! Voluptatibus placeat ratione a suscipit cumque explicabo minus.",
-      price: "Rp. 1.000.000",
+      price: 1000000,
     },
     {
+      key: Math.random() * 100,
       id: 2,
       title: "Shoes Aliodas",
       image: shoesAliodas,
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat voluptates temporibus debitis vitae architecto quas quia deserunt voluptatum enim labore odit eaque! Voluptatibus.",
-      price: "Rp. 3.000.000",
+      price: 3000000,
     },
     {
+      key: Math.random() * 100,
       id: 3,
       title: "Hat Nike",
       image: hat,
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat voluptates temporibus debitis vitae architecto quas quia deserunt voluptatum enim.",
-      price: "Rp. 350.000",
+      price: 350000,
     },
   ];
+
+  const [cart, setCart] = useState([
+    {
+      key: Math.random() * 20,
+      id: 1,
+      qty: 1,
+    },
+  ]);
+
+  function handleAddToCart(id) {
+    setCart([
+      ...cart,
+      {
+        key: Math.random() * 20,
+        id: id,
+        qty: 1,
+      },
+    ]);
+  }
+
   function handleLogout() {
     window.location.href = "/";
     localStorage.clear();
@@ -54,11 +78,11 @@ export default function Products() {
         </div>
       </div>
       <div className="w-[95%] mx-auto">
-        <div className="flex justify-between mt-5 gap-5">
+        <div className="flex justify-between mt-5">
           <div className="w-full basis-2/3">
-            <div className="flex flex-wrap ml-5 gap-4">
+            <div className="flex flex-wrap gap-4">
               {products.map((product) => (
-                <CardProduct key={product.id}>
+                <CardProduct key={product.key}>
                   <CardProduct.Header
                     title={product.title}
                     image={product.image}
@@ -66,14 +90,40 @@ export default function Products() {
                   <CardProduct.Body>{product.desc}</CardProduct.Body>
                   <CardProduct.Footer
                     price={product.price}
-                    toDetailProduct="/productDetail"
+                    handleAddToCart={handleAddToCart}
+                    id={product.id}
                   />
                 </CardProduct>
               ))}
             </div>
           </div>
           <div className="basis-1/3">
-            <h1>hallo</h1>
+            <h1 className="text-xl font-semibold mb-3">Cart</h1>
+            <table className="text-left table-auto border-separate border-spacing-x-4 border-spacing-y-1">
+              <thead>
+                <tr>
+                  <td>Qty</td>
+                  <td>Product Name</td>
+                  <td>Price</td>
+                  <td>Total</td>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((item) => {
+                  const product = products.find(
+                    (product) => product.id === item.id
+                  );
+                  return (
+                    <tr key={item.key}>
+                      <td>{item.id}</td>
+                      <td>{product.title}</td>
+                      <td>{product.price}</td>
+                      <td>{item.qty * product.price}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
